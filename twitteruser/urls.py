d1a2258052917aec97f2d7b2397f859e4beffdc2
James Maupin <jmsMaupinRC@gmail.com>
2020-02-27 15:51:53 -0500

    Add ability to follow and unfollow users

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from twitteruser import views
 
 urlpatterns = [
-    path('user/<int:user_id>', views.user_detail_view)
+    path('user/<int:user_id>', views.user_detail_view),
+    path('follow/<int:user_id>', views.follow_user_view),
+    path('unfollow/<int:user_id>', views.unfollow_user_view)
 ]
--- twitteruser/views.py
+++ twitteruser/views.py
@@ -11,11 +11,44 @@ def user_detail_view(request, user_id):
         user = TwitterUser.objects.get(id=user_id)
         users = TwitterUser.objects.all()
         tweets = Tweet.objects.filter(created_by=user)
+
+        cur_user = TwitterUser.objects.get(id=request.user.id)
+        is_following = cur_user.following.filter(id=user_id).exists()
     except Exception:
         return HttpResponseRedirect(reverse('homepage'))
 
     return render(request, 'profile.html', {
         'user': user,
         'tweets': tweets,
-        'users': users
+        'users': users,
+        'is_following': is_following
     })
+
+
+def follow_user_view(request, user_id):
+    user_to_follow = None
+    current_user = None
+    try:
+        user_to_follow = TwitterUser.objects.get(id=user_id)
+        current_user = TwitterUser.objects.get(id=request.user.id)
+
+        current_user.following.add(user_to_follow)
+    except Exception:
+        pass
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def unfollow_user_view(request, user_id):
+    user_to_unfollow = None
+    current_user = None
+
+    try:
+        user_to_unfollow = TwitterUser.objects.get(id=user_id)
+        current_user = TwitterUser.objects.get(id=request.user.id)
+
+        current_user.following.remove(user_to_unfollow)
+    except Exception:
+        pass
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
